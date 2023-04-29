248e3d133187d5e2ce4c16aed10c62aaa7a31fd0
Marc Fontova <mfontovat@gmail.com>
2023-04-29 19:02:56 +0200

    1.6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,8 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
+
+const Header = ({text}) => {
+  return (
+    <h1>{text}</h1>
+  )
+}
+
+const Button = ({text, onClick}) => {
+  return (
+    <button onClick={onClick} >{text}</button>
+  )
+}
+
+const Statistics = ({text, number}) => {
+  return (
+    <>
+      <p>{text}: {number}</p>
+    </>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const addGood = () => {
+    setGood(good + 1)
+  }
+  const addNeutral = () => {
+    setNeutral(neutral + 1)
+  }
+  const addBad = () => {
+    setBad(bad + 1)
+  }
+
+  return (
+    <div>
+      <Header text="give feedback" />
+      <Button text="Good" onClick={addGood} />
+      <Button text="Neutral" onClick={addNeutral} />
+      <Button text="Bad" onClick={addBad} />
+      <Header text="statistics" />
+      <Statistics text="good" number={good} />
+      <Statistics text="neutral" number={neutral} />
+      <Statistics text="bad" number={bad} />
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +57,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
