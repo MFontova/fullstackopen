11b7a34c5da6cdbcc8bbf32479aa1349b5be2008
Marc Fontova <mfontovat@gmail.com>
2023-05-05 20:06:46 +0200

    2.11

--- part2/phoneguide/src/App.js
+++ part2/phoneguide/src/App.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '666555444' },
-    { name: 'Marc Fontova', number: '666333999' },
-  ]) 
+  
+  const [ persons, setPersons ] = useState([]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newSearch, setNewSearch ] = useState('')
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  })
 
   const inputNameHandler = (event) =>{
     const input = event.target.value
